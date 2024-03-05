13930f95d80b5d3f2526758ce4a2c7b295dca91e
Justin Mak <j.mak.dev@gmail.com>
2024-03-04 22:49:56 -0800

    Fix result calculation logic

--- app/routes/game/game.server.test.ts
+++ app/routes/game/game.server.test.ts
@@ -2,15 +2,23 @@ import { calculateResult } from './game.server';
 
 it('should return the correct results in a sorted array from correct (1) to incorrect (-1)', () => {
   // Partial
-  const code = [1, 2, 3, 4];
+  let code = [1, 2, 3, 4];
   let submission = [4, 2, 1, 0];
   expect(calculateResult(code, submission)).toEqual([1, 0, 0, -1]);
+
+  // All right
+  submission = [1, 2, 3, 4];
+  expect(calculateResult(code, submission)).toEqual([1, 1, 1, 1]);
 
   // All wrong
   submission = [5, 0, 5, 0];
   expect(calculateResult(code, submission)).toEqual([-1, -1, -1, -1]);
 
-  // All right
-  submission = [1, 2, 3, 4];
-  expect(calculateResult(code, submission)).toEqual([1, 1, 1, 1]);
+  // Duplicates
+  submission = [1, 1, 3, 3];
+  expect(calculateResult(code, submission)).toEqual([1, 1, -1, -1]);
+
+  // Double duplicates
+  code = [1, 3, 1, 3];
+  expect(calculateResult(code, submission)).toEqual([1, 1, 0, 0]);
 });
--- app/routes/game/game.server.ts
+++ app/routes/game/game.server.ts
@@ -105,6 +105,17 @@ export async function createGame(userId?: string) {
   return toParsedGame(game);
 }
 
+/**
+ * Calculates the results and returns an array of numbers that represent the 3
+ * possible states:
+ *
+ * 1: Correct color and spot
+ * 0: Correct color
+ * -1: Incorrect
+ *
+ * To keep it in a single loop, we just flip the code value to negative when a
+ * match is found.
+ */
 export function calculateResult(code: number[], submission: number[]) {
   const result: number[] = Array(code.length);
   const temp = [...code];
@@ -112,14 +123,14 @@ export function calculateResult(code: number[], submission: number[]) {
   for (let i = 0; i < submission.length; i++) {
     if (code[i] === submission[i]) {
       result[i] = RESULT_MAP.correctColorAndSpot;
-      temp.splice(i, 1);
+      temp[i] *= -1;
       continue;
     }
 
     const misplacedIndex = temp.indexOf(submission[i]);
     if (misplacedIndex > -1) {
       result[i] = RESULT_MAP.correctColor;
-      temp.splice(misplacedIndex, 1);
+      temp[misplacedIndex] *= -1;
       continue;
     }
 
