b06efd9e8dc698a6faf8946a18b8ce3b6395afca
evanmalmud <evanmalmud@gmail.com>
2024-05-03 23:52:05 -0400

    Update entry.server.tsx

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,15 +1,14 @@
-/* eslint-disable import/order */
-
 /**
  * By default, Remix will handle generating the HTTP Response for you.
  * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
  * For more information, see https://remix.run/file-conventions/entry.server
  */
-import type { AppLoadContext, EntryContext } from '@remix-run/node';
+import { PassThrough } from 'node:stream';
+
+import type { EntryContext , AppLoadContext } from '@remix-run/node';
 import { createReadableStreamFromReadable } from '@remix-run/node';
 import { RemixServer } from '@remix-run/react';
 import { isbot } from 'isbot';
-import { PassThrough } from 'node:stream';
 import { renderToPipeableStream } from 'react-dom/server';
 
 const ABORT_DELAY = 5_000;
