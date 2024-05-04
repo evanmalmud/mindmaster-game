c6f5775664c62bfcccf4e1070d0a6a800c0ff79a
evanmalmud <evanmalmud@gmail.com>
2024-05-03 23:34:53 -0400

    fixing ES lint

--- app/components/ui/dialog.tsx
+++ app/components/ui/dialog.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import * as DialogPrimitive from '@radix-ui/react-dialog';
 import { Cross2Icon } from '@radix-ui/react-icons';
 import * as React from 'react';
--- app/components/ui/input.tsx
+++ app/components/ui/input.tsx
@@ -1,6 +1,9 @@
-import * as React from "react"
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
 
-import { cn } from "~/utils"
+/* eslint-disable @typescript-eslint/no-empty-interface */
+import * as React from 'react';
+
+import { cn } from '~/utils';
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
@@ -11,15 +14,15 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-          className
+          'flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
+          className,
         )}
         ref={ref}
         {...props}
       />
-    )
-  }
-)
-Input.displayName = "Input"
+    );
+  },
+);
+Input.displayName = 'Input';
 
-export { Input }
+export { Input };
